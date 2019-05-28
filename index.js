756aed7ccf43bc522189618d9cdeeba77dd73200
kranthi kumar <kranthi.8t7@gmail.com>
2019-05-28 19:26:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var recipes = new Object ({})
+var recipes = new Object ({});
 
  var obj = { prop: 1 }
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},obj,{[key]:value})
-}
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, obj, {[key]: value})
+};
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
-}
-function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return obj
-}
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
+};
+var newObj = Object.assign({}, obj);
+function deleteFromObjectByKey(object, key){
+  delete newObj[key];
+  return newObj
+};
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
   return object
 }
